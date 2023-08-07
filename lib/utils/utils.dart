0b6cdd25ba762b3c73c5294a86a0a20782eae60b
shahkutub <shah.kutub1@gmail.com>
2023-08-07 18:03:16 +0600

    check internet

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,8 +1,23 @@
+import 'dart:io';
+
 class Utils{
   static bool isValidateEmail(String email) {
     final bool emailValid =
     RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
         .hasMatch(email);
     return emailValid;
+  }
+
+  Future<bool> checkNetwork() async {
+    bool isConnected = false;
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        isConnected = true;
+      }
+    } on SocketException catch (_) {
+      isConnected = false;
+    }
+    return isConnected;
   }
 }
